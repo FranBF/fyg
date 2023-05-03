df5fa5af4a9df9d4b5563ebd648d93fb343ef3ff
Fran Benages Fernandez <franbenagesfernandez@MBP-de-Fran.home>
2023-05-03 22:51:12 +0200

    build 3

--- src/pages/Madrid.jsx
+++ src/pages/Madrid.jsx
@@ -1,7 +1,7 @@
 import imageRetiroLeones from '../public/ImagenesGemma/MadridRetiroLeones.jpg'
 import imageCibelesBeso from '../public/ImagenesGemma/MadridCibelesBeso.jpg'
 import imageCibelesJuntos from '../public/ImagenesGemma/MadridCibelesJuntos.jpg'
-import imageCibeles from '../public/ImagenesGemma/cibeles.jpg'
+import imageCibeles from '../public/ImagenesGemma/Cibeles.jpg'
 import imagePinta from '../public/ImagenesGemma/Pinta.jpg'
 import imageRechulones from '../public/ImagenesGemma/Rechulones.jpg'
 import { Slider } from '../components/Slider'
